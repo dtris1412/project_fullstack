06c7b591a9cb119b819e1a03d9ac9021786de146
dtris <daohuutri04@gmail.com>
2025-03-04 16:32:35 +0700

    connect db

--- src/server.js
+++ src/server.js
@@ -2,14 +2,18 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
+import connectDB from "./config/connectdb";
 require("dotenv").config();
 
 let app = express();
 //config app
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 viewEngine(app);
 initWebRoutes(app);
+
+connectDB();
 let port = process.env.PORT || 8081;
 let hostname = process.env.HOST_NAME;
 app.listen(port, hostname, () => {
